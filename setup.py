d5491990a360a7ba8f85053054af924a68ff2fbf
rahilwazir <rahilwazir@hotmail.com>
2018-03-15 11:28:52 +0500

    published to pypi

--- setup.py
+++ setup.py
@@ -3,6 +3,10 @@ from setuptools import setup
 setup(
     name='synym',
     version='0.1',
+    description='CLI tool to list synonyms for a word from thesaurus.com',
+    author='Rahil Wazir',
+    author_email='rahilwazirali@gmail.com',
+    url='https://github.com/rahilwazir/synym',
     py_modules=['synym'],
     install_requires=[
         'Click',
@@ -11,6 +15,7 @@ setup(
     dependency_links=[
         'https://github.com/rahilwazir/thesaurus-api/tarball/master#egg=thesaurus-0.1'
     ],
+    download_url='https://github.com/rahilwazir/synym/archive/0.1.tar.gz',
     entry_points='''
         [console_scripts]
         synym=synym:cli
